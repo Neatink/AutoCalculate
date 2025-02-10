d3fdae84bca4c23ecddfb9e3d6f817c24ba9e370
vladislavchik42 <minyaylo51@gmail.com>
2025-02-10 21:02:46 +0200

    Improved text recognition,Removed extra spaces

--- main.py
+++ main.py
@@ -3,68 +3,57 @@ import time
 import mss,mss.tools
 import os
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 import re
-
 print("Press F7 to screenshot/Нажми F7 для скриншота.")
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 os.makedirs(r'C:\Windows\Temp', exist_ok=True)
+def upgrade_image(image_path):
+    img = Image.open(image_path)
+    img = img.convert('L')
+    img = img.filter(ImageFilter.SHARPEN)
+    img = ImageEnhance.Contrast(img).enhance(2)
+    img = img.point(lambda x: 0 if x < 140 else 255)
+    return img
 
 def doScreen():
     with mss.mss() as sct:
-
         monitor = {"top": 60, "left": 0, "width": 650, "height": 120}
-
         output=r"C:\Windows\Temp\\screenshot.png".format(**monitor)
         sct_img = sct.grab(monitor)
-
         mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-
         return output
 
-
-
 def Solution_Example(image_path):
-    img = Image.open(image_path)
-    text = pytesseract.image_to_string(img, lang="eng+rus")
-
-
+    img = upgrade_image(image_path)
+    text = pytesseract.image_to_string(img, lang="eng+rus+ukr")
     match = re.search(r"(\d+)\s*\+\s*(\d+)", text)
     if match:
         num1, num2 = int(match.group(1)), int(match.group(2))
         return num1 + num2
     return None
 
-
-
 def tupping_answer(answer):
     controller = keyboard.Controller()
-
     controller.press("y")
     controller.release("y")
-
     time.sleep(0.1)
-
     controller.type(str(answer))
-
-
     controller.press(keyboard.Key.enter)
     controller.release(keyboard.Key.enter)
-
 
 def on_press(key):
     global answer
     if key == keyboard.Key.f7:
         screenshot_path = doScreen()
         answer = Solution_Example(screenshot_path)
-        if answer != None:
+        if answer is not None:
             print(f"Ответ: {answer}")
             tupping_answer(answer)
         else:
             print("Не увидел примера")
-
 
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
