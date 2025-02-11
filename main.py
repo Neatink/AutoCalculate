bd484a6b8348627d923e011384794b01a0fd28db
vladislavchik42 <minyaylo51@gmail.com>
2025-02-11 18:44:30 +0200

    Add new mod

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import os
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 import re
-print("Press F7 to screenshot/Нажми F7 для скриншота.")
+import threading
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+os.makedirs(r'C:\\Windows\\Temp', exist_ok=True)
 
-os.makedirs(r'C:\Windows\Temp', exist_ok=True)
 def upgrade_image(image_path):
-    img = Image.open(image_path)
-    img = img.convert('L')
+    img = Image.open(image_path).convert('L')
     img = img.filter(ImageFilter.SHARPEN)
     img = ImageEnhance.Contrast(img).enhance(2)
     img = img.point(lambda x: 0 if x < 140 else 255)
@@ -21,7 +20,7 @@ def upgrade_image(image_path):
 def doScreen():
     with mss.mss() as sct:
         monitor = {"top": 60, "left": 0, "width": 650, "height": 120}
-        output=r"C:\Windows\Temp\\screenshot.png".format(**monitor)
+        output=r"C:\\Windows\\Temp\\screenshot.png"
         sct_img = sct.grab(monitor)
         mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
         return output
@@ -39,21 +38,44 @@ def tupping_answer(answer):
     controller = keyboard.Controller()
     controller.press("y")
     controller.release("y")
-    time.sleep(0.1)
+    time.sleep(0.2)
     controller.type(str(answer))
     controller.press(keyboard.Key.enter)
     controller.release(keyboard.Key.enter)
 
-def on_press(key):
-    global answer
-    if key == keyboard.Key.f7:
-        screenshot_path = doScreen()
-        answer = Solution_Example(screenshot_path)
-        if answer is not None:
-            print(f"Ответ: {answer}")
-            tupping_answer(answer)
-        else:
-            print("Не увидел примера")
+def always_screen():
+    answer = Solution_Example(doScreen())
+    if answer is not None:
+        print(f"Ответ: {answer}")
+        tupping_answer(answer)
+    else:
+        print("Не увидел примера")
 
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+    threading.Timer(2.0, always_screen).start()
+    time.sleep(0.1)
+
+def bind_screen():
+    print("\nPress F7 to screenshot | Нажми F7 для скриншота.")
+    def on_press(key):
+        if key == keyboard.Key.f7:
+            answer = Solution_Example(doScreen())
+            if answer is not None:
+                print(f"Ответ: {answer}")
+                tupping_answer(answer)
+            else:
+                print("Не увидел примера")
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    try:
+        question = int(input("Всегда(BETA)(1) | По бинду(2)\nAlways(BETA)(1) | Bind(2): "))
+        if question == 1:
+            always_screen()
+        elif question == 2:
+            bind_screen()
+        else:
+            print("Есть варианты: 1 или 2")
+    except ValueError as error:
+        print(f"Писать можно только числа!")
+
